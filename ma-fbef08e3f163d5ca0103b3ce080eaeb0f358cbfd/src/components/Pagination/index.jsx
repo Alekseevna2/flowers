bc8d00d2e8baaff0b3cbf8c32298ea78533d2048
Alekseevna2 <124335289+Alekseevna2@users.noreply.github.com>
2024-12-10 17:13:41 +0500

    Update index.jsx
    000

--- ma-fbef08e3f163d5ca0103b3ce080eaeb0f358cbfd/src/components/Pagination/index.jsx
+++ ma-fbef08e3f163d5ca0103b3ce080eaeb0f358cbfd/src/components/Pagination/index.jsx
@@ -1,20 +1,20 @@
-// import React from 'react'
-// import Paginate from './Pagination'
-// import styles from './Pagination.module.scss'
+import React from 'react'
+import Paginate from './Pagination'
+import styles from './Pagination.module.scss'
 
-// const Pagination=({ onChangePage }) =>{
-//   return (
-//     <Paginate
-//       className = {styles.root}
-//       breaklabel = "..."
-//       nextLabel = ">"
-//       previousLabel = "<"
-//       onPageChange={event => onChangePage(event.selected + 1)}
-//       pageRangeDisplayed = {4}
-//       pageCount = {3}
-//       renderOnZeroPageCount = {null}
-//     />
-//   )
-// }
+const Pagination=({ onChangePage }) =>{
+  return (
+    <Paginate
+      className = {styles.root}
+      breaklabel = "..."
+      nextLabel = ">"
+      previousLabel = "<"
+      onPageChange={event => onChangePage(event.selected + 1)}
+      pageRangeDisplayed = {4}
+      pageCount = {3}
+      renderOnZeroPageCount = {null}
+    />
+  )
+}
 
-// export default Pagination;
+export default Pagination;
